f3ee9452b2189793c753d22b9a5a3748bedc60ca
tuan <phamtuan2xx3@gmail.com>
2025-03-31 17:36:11 +0700

    first commit

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Text('This is Profile Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Text('Settings Screen Content'),
+      ),
+    );
+  }
+}
